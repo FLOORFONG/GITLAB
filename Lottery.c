2411e05ae786cf80cf86cb6cb2d7eaf33d1f72eb
FLOORFONG <fong.narudejt@gmail.com>
2017-04-24 14:22:57 +0700

    change Lottery.c at 2 places

--- Lottery.c
+++ Lottery.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 void main()
 {
-  int WinLotteryNo[10] = {100000000000, 100000000000, 5515, 7233, 5614, 8876, 1215, 4232, 9155, 9995}; 
-  float WinAmount[10] = {100000000000, 100000000000, 10000, 5000, 5000, 5000, 5000, 3000, 3000, 3000};
+  int WinLotteryNo[10] = {99999999999, 99999999999, 5515, 7233, 5614, 8876, 1215, 4232, 9155, 9995}; 
+  float WinAmount[10] = {99999999999, 99999999999, 10000, 5000, 5000, 5000, 5000, 3000, 3000, 3000};
   int N = 10;
   int i, won; 
   
@@ -10,7 +10,7 @@ void main()
    
   printf("Enter your Lottery Number (0 to end): "); 
   scanf("%d", &YourLotteryNo); 
-  while (YourLotteryNo != 0) 
+  while (YourLotteryNo != 999999999999) 
   { 
     won = 0; 
     for (i=0;i<N && !won; i++) 
